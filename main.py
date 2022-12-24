bc585b896068648b647e223ff0ca0833dc7097df
rafat86 <115078051+rafat86@users.noreply.github.com>
2022-12-25 00:26:45 +0200

    8 th commit
    code 3

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ df3 = crime_spain_df[["Year", "Crime", "Location", "Total cases"]].groupby("Crim
 df4 = df2[["Total cases"]].sum()
 crime = df3["Crime"]
 
-
+'''
 def trend_chart(crime_case):
     year = list(set(crime_spain_df["Year"]))
     total_cases = (df4.iloc[crime_case-1, 0], df4.iloc[13 + crime_case, 0], df4.iloc[27 + crime_case, 0])
     plt.plot(year, total_cases)
     plt.xlabel('Years')
     plt.ylabel('Total cases')
-    plt.title(crime_spain_df.iloc[crime_case-1, 2])
+    plt.title(sorted_crime_df.iloc[crime_case-1, 2])
     return plt.show()
 
 
@@ -61,3 +61,56 @@ elif reader_plot == 3:
     print("plot 3")
 else:
     reader_plot = int(input("Please select the type of chart you want to plot: "))
+'''
+i = 0
+for crime in crime:
+    if i in range(0, 15):
+        print(i+1, "-", sorted_crime_df.iloc[i, 2])
+    i = i + 1
+
+reader_crime_1 = int(input("select first crime in bar plot:  "))
+reader_crime_2 = int(input("select Second crime in bar plot:  "))
+reader_crime_3 = int(input("select third crime in bar plot:  "))
+
+print("Names of the cities:", "\n", "Madrid", "Barcelona", "Valencia", "Murcia", "\n",
+      "Seville", "Zaragoza", "Malaga", "Alicante", "\n", "Cordoba", "Granada", "Rioja", "Salamanca",
+      "\n", "Segovia", "Ceuta", "Melilla", "Spain")
+
+reader_city_1 = input("Type a name of first city in capital letter:  ")
+reader_city_2 = input("Type a name of second city in capital letter:  ")
+reader_city_3 = input("Type a name of third city in capital letter:  ")
+reader_city_4 = input("Type a name of fourth city in capital letter:  ")
+reader_city_5 = input("Type a name of fifth city in capital letter:  ")
+
+first_crime = sorted_crime_df.iloc[reader_crime_1-1, 2]
+second_crime = sorted_crime_df.iloc[reader_crime_2-1, 2]
+third_crime = sorted_crime_df.iloc[reader_crime_3-1, 2]
+
+reader_year = int(input("select a year (2019, 2020, 2021:  "))
+
+count_crime_1 = int(crime_spain_df.loc[(reader_city_1, reader_year, first_crime), 'Total cases'])
+count_crime_2 = 100
+count_crime_3 = 200
+count_crime_4 = 500
+count_crime_5 = 1000
+
+city_labels = [reader_city_1, reader_city_2, reader_city_3, reader_city_4, reader_city_5]
+first_crime_count = [count_crime_1, count_crime_2, count_crime_3, count_crime_4, count_crime_5]
+second_crime_count = [170, 270, 290, 150, 165]
+third_crime_count = [170, 270, 290, 150, 165]
+
+print(city_labels)
+
+x = np.arange(len(city_labels))
+width = 0.2
+fig, ax = plt.subplots()
+ax.bar(x-width, first_crime_count, width, label=first_crime, color="red")
+ax.bar(x+width, second_crime_count, width, label=second_crime, color="blue")
+ax.bar(x, third_crime_count, width, label=third_crime, color="green")
+ax.set_ylabel('Crimes in Spain in')
+ax.set_title('Number of crimes per city')
+ax.set_xticks(x)
+ax.set_xticklabels(city_labels, rotation='vertical')
+ax.legend()
+fig.tight_layout()
+plt.show()
